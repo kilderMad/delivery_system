f569a84d16771a6b0e21aae2ac06728abb2d047c
kilderMad <costa.kilder@live.com>
2022-05-21 09:36:32 -0300

    att price tests

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -3,38 +3,33 @@ require 'rails_helper'
 RSpec.describe Price, type: :model do
   describe '#valid' do
     it 'Metros cúbicos min not be blank' do
-      carrier = Carrier.create!(fantasy_name: 'DLL', cnpj: '12345678901237', domain: 'dll.com.br', address: 'Av. Geraldo Patrin, 745', email: 'support@dll.com.br')
-      price = Price.new(cbm_min: '', cbm_max: 0.500, weight_min: 0, weight_max: 10, value_km: 0.5, carrier: carrier)
-      
-      expect(price.valid?).to eq false
+      price = Price.new(cbm_min: '')
+      price.valid? 
+      expect(price.errors.include? :cbm_min).to eq true
     end
 
     it 'Metros cúbicos max not be blank' do
-      carrier = Carrier.create!(fantasy_name: 'DLL', cnpj: '12345678901237', domain: 'dll.com.br', address: 'Av. Geraldo Patrin, 745', email: 'support@dll.com.br')
-      price = Price.new(cbm_min: 0.001, cbm_max: '', weight_min: 0, weight_max: 10, value_km: 0.5, carrier: carrier)
-
-      expect(price.valid?).to eq false
+      price = Price.new(cbm_max: '')
+      price.valid? 
+      expect(price.errors.include? :cbm_max).to eq true
     end
 
     it 'Peso min not be blank' do
-      carrier = Carrier.create!(fantasy_name: 'DLL', cnpj: '12345678901237', domain: 'dll.com.br', address: 'Av. Geraldo Patrin, 745', email: 'support@dll.com.br')
-      price = Price.new(cbm_min: 0.001, cbm_max: 0.500, weight_min: '', weight_max: 10, value_km: 0.5, carrier: carrier)
-
-      expect(price.valid?).to eq false
+      price = Price.new(weight_min: '')
+      price.valid?
+      expect(price.errors.include? :weight_min).to eq true
     end
 
     it 'Peso max not be blank' do
-      carrier = Carrier.create!(fantasy_name: 'DLL', cnpj: '12345678901237', domain: 'dll.com.br', address: 'Av. Geraldo Patrin, 745', email: 'support@dll.com.br')
-      price = Price.new(cbm_min: 0.001, cbm_max: 0.500, weight_min: 0, weight_max: '', value_km: 0.5, carrier: carrier)
-
-      expect(price.valid?).to eq false
+      price = Price.new(weight_max: '')
+      price.valid?
+      expect(price.errors.include? :weight_max).to eq true
     end
 
     it 'Valor por km not be blank' do
-      carrier = Carrier.create!(fantasy_name: 'DLL', cnpj: '12345678901237', domain: 'dll.com.br', address: 'Av. Geraldo Patrin, 745', email: 'support@dll.com.br')
-      price = Price.new(cbm_min: 0.001, cbm_max: 0.500, weight_min: 0, weight_max: 10, value_km: '', carrier: carrier)
-
-      expect(price.valid?).to eq false
+      price = Price.new(value_km: '')
+      price.valid?
+      expect(price.errors.include? :value_km).to be true
     end
 
   end
