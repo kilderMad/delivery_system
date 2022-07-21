34f9dc1ac5799c6152b4e7bf0e2f143259a3f584
killer-cf <costa.kilder@live.com>
2022-07-21 16:52:23 -0300

    implementado react-router

---
+++ app/javascript/react/src/components/Menu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+
+export const Menu = () => {
+  return (
+    <header>
+      <nav id="menu" className=" flex justify-between">
+        <div className="flex items-center">
+          <div className="item">
+            <h2>DeliveryFast</h2>
+          </div>
+          <Link to="/">Home</Link>
+          <Link to="/carries">Transportadoras</Link>
+          <Link to="/orders">Pedidos</Link>
+          <Link to="/">Dashboard</Link>
+          <Link to="/carrier/carrier_id/prices">Tabela de preços</Link>
+          <Link to="/">Tabela de prazos</Link>
+          <Link to="/orders">Pedidos</Link>
+        </div>
+        <div className="flex">
+          <div className="px-4">Current-user</div>
+          <a path="/users/sign_in">Entrar</a>
+        </div>
+        <Button path="/users/sign_out" method="delete" title="Sair" />
+      </nav>
+    </header>
+  );
+};
